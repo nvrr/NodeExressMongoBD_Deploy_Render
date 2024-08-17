4caa8e2ebd2b5f9b7367541489f836a6c1894438
nvrr <sureshvarma809@gmail.com>
2024-08-18 03:23:23 +0530

    MDB CONNECTED

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
 
 //routes
 app.get('/', (req, res) => {
     res.send('helooo here from backend')
 })
 
-app.listen(3000, ()=> {
+app.get('/blog', (req,res) => {
+    res.send('Helo Bloger')
+})
+mongoose.set("strictQuery", false)
+
+mongoose.connect('mongodb+srv://nvr:1a1b1c@admin.2jdet.mongodb.net/Node-API?retryWrites=true&w=majority&appName=admin')
+  .then(() => {
+  console.log('Connected to MongoDB!')
+  app.listen(3000, ()=> {
     console.log('node runing onn port 3000');
 } )
+}).catch((error) => {
+    console.log(error);
+})
+
+
+
